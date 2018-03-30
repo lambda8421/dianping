851df2690b148ddee466bb68eaa1991a2554373b
Yongzhao Zhang <lambda8421@163.com>
2018-03-30 00:49:44 -0500

    header and header Ads page in home page

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-import { Link, hashHistory } from 'react-router'
-
-import SearchInput from '../SearchInput'
 
 import './style.less'
 
@@ -15,11 +12,9 @@ class HomeHeader extends React.Component {
         return (
             <div id="home-header" className="clear-fix">
                 <div className="home-header-left float-left">
-                    <Link to="/city">
-                        <span>{this.props.cityName}</span>
-                        &nbsp;
-                        <i className="icon-angle-down"></i>
-                    </Link>
+                    <span>{this.props.cityName}</span>
+                    &nbsp;
+                    <i className="icon-angle-down"></i>
                 </div>
                 <div className="home-header-right float-right">
                     <i className="icon-user"></i>
@@ -27,15 +22,11 @@ class HomeHeader extends React.Component {
                 <div className="home-header-middle">
                     <div className="search-container">
                         <i className="icon-search"></i>
-                        &nbsp;
-                        <SearchInput value="" enterHandle={this.enterHandle.bind(this)}/>
+                        <input type="text" placeholder="请输入关键字"/>
                     </div>
                 </div>
             </div>
         )
-    }
-    enterHandle(value) {
-        hashHistory.push('/search/all/' + encodeURIComponent(value))
     }
 }
 
